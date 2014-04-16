736b53041cca95ff92c6420c58fd32d4328dfc88
Robert Brignull <robert.brignull@gmail.com>
2014-04-17 00:28:30 +0100

    Added tests for moving sections.

--- V3/src/test/section.cpp
+++ V3/src/test/section.cpp
@@ -237,6 +237,311 @@ void SectionTest::test_editSection()
     QVERIFY(factList->idChildFactMap.size() == 0);
 }
 
+void SectionTest::test_editSectionOrdering_moveMode()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *sectionName = "section 1";
+
+    // Add the course and section
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName, "Section");
+
+    // Check that the correct buttons are visible
+    auto factList = coursePage->factListView->currentFactList->idChildSectionMap.begin()->second;
+    QVERIFY(factList->moveButton->isHidden() == false);
+    QVERIFY(factList->moveAboveButton->isHidden() == true);
+    QVERIFY(factList->moveBelowButton->isHidden() == true);
+
+    // Click the move button
+    QTest::mouseClick(factList->moveButton, Qt::LeftButton);
+
+    // Check that the other buttons are visible
+    QVERIFY(factList->moveButton->isHidden() == true);
+    QVERIFY(factList->moveAboveButton->isHidden() == false);
+    QVERIFY(factList->moveBelowButton->isHidden() == false);
+
+    // Click the moveAboveButton
+    QTest::mouseClick(factList->moveAboveButton, Qt::LeftButton);
+
+    // Check that the original buttons are visible
+    QVERIFY(factList->moveButton->isHidden() == false);
+    QVERIFY(factList->moveAboveButton->isHidden() == true);
+    QVERIFY(factList->moveBelowButton->isHidden() == true);
+
+    // Click the move button
+    QTest::mouseClick(factList->moveButton, Qt::LeftButton);
+
+    // Check that the other buttons are visible
+    QVERIFY(factList->moveButton->isHidden() == true);
+    QVERIFY(factList->moveAboveButton->isHidden() == false);
+    QVERIFY(factList->moveBelowButton->isHidden() == false);
+
+    // Click the moveBelowButton
+    QTest::mouseClick(factList->moveAboveButton, Qt::LeftButton);
+
+    // Check that the original buttons are visible
+    QVERIFY(factList->moveButton->isHidden() == false);
+    QVERIFY(factList->moveAboveButton->isHidden() == true);
+    QVERIFY(factList->moveBelowButton->isHidden() == true);
+}
+
+void SectionTest::test_editSectionOrdering_moveAbove()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *sectionName1 = "section 1";
+    const char *sectionName2 = "section 2";
+
+    // Add the course and sections
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName1, "Section");
+    TestUtil::addFact(window, sectionName2, "Section");
+
+    // Move the second proof above the first
+    auto currentFactListLayout = coursePage->factListView->currentFactList->layout;
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(1)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(0)->widget())->moveAboveButton, Qt::LeftButton);
+
+    // Check that the proofs are now in the correct order
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.name == sectionName2);
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.name == sectionName1);
+
+    // Check that the orderings are valid
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.ordering < ((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.ordering);
+}
+
+void SectionTest::test_editSectionOrdering_moveBelow()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *sectionName1 = "section 1";
+    const char *sectionName2 = "section 2";
+
+    // Add our course and sections
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName1, "Section");
+    TestUtil::addFact(window, sectionName2, "Section");
+
+    // Move the first proof below the second
+    auto currentFactListLayout = coursePage->factListView->currentFactList->layout;
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(1)->widget())->moveBelowButton, Qt::LeftButton);
+
+    // Check that the proofs are now in the correct order
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.name == sectionName2);
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.name == sectionName1);
+
+    // Check that the orderings are valid
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.ordering < ((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.ordering);
+}
+
+void SectionTest::test_editSectionOrdering_moveBetween()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *sectionName1 = "section 1";
+    const char *sectionName2 = "section 2";
+    const char *sectionName3 = "section 3";
+
+    // Add our course and sections
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName1, "Section");
+    TestUtil::addFact(window, sectionName2, "Section");
+    TestUtil::addFact(window, sectionName3, "Section");
+
+    // Move the first proof below the second
+    auto currentFactListLayout = coursePage->factListView->currentFactList->layout;
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(((FactList*) currentFactListLayout->itemAt(1)->widget())->moveBelowButton, Qt::LeftButton);
+
+    // Check that the proofs are now in the correct order
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.name == sectionName2);
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.name == sectionName1);
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(2)->widget())->fact.name == sectionName3);
+
+    // Check that the orderings are valid
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(0)->widget())->fact.ordering < ((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.ordering);
+    QVERIFY(((FactList*) currentFactListLayout->itemAt(1)->widget())->fact.ordering < ((FactList*) currentFactListLayout->itemAt(2)->widget())->fact.ordering);
+}
+
+void SectionTest::test_editSectionOrdering_factIntoSection()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *level0FactName = "level 0 fact";
+    const char *level0FactType = "Axiom";
+    const char *level1FactName = "level 1 fact";
+    const char *level1FactType = "Axiom";
+    const char *sectionName = "section";
+
+    // Add our course, section and facts
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, level0FactName, level0FactType);
+    TestUtil::addFact(window, sectionName, "Section");
+    TestUtil::addFactToSection(window, level1FactName, level1FactType, sectionName);
+
+    // Move the outer fact inside the section
+    auto factList = coursePage->factListView->currentFactList;
+    QTest::mouseClick(((ExpandingFactWidget*) factList->layout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(factList->idChildSectionMap.begin()->second->idChildFactMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Check that the fact has moved
+    QVERIFY(factList->layout->count() == 1);
+    QVERIFY(factList->idChildMap.size() == 1);
+    QVERIFY(factList->idChildSectionMap.size() == 1);
+    QVERIFY(factList->idChildFactMap.size() == 0);
+
+    factList = factList->idChildSectionMap.begin()->second;
+    QVERIFY(factList->layout->count() == 3);
+    QVERIFY(factList->idChildMap.size() == 2);
+    QVERIFY(factList->idChildSectionMap.size() == 0);
+    QVERIFY(factList->idChildFactMap.size() == 2);
+
+    // Check that the facts are in the correct order
+    QVERIFY(((ExpandingFactWidget *) factList->layout->itemAt(1)->widget())->fact.name == level0FactName);
+    QVERIFY(((ExpandingFactWidget *) factList->layout->itemAt(2)->widget())->fact.name == level1FactName);
+}
+
+void SectionTest::test_editSectionOrdering_sectionIntoSection()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *factName = "fact";
+    const char *factType = "Axiom";
+    const char *sectionName0 = "section 0";
+    const char *sectionName1 = "section 1";
+
+    // Add our course, section and facts
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName0, "Section");
+    TestUtil::addFact(window, sectionName1, "Section");
+    TestUtil::addFactToSection(window, factName, factType, sectionName1);
+
+    // Move one section inside the other section
+    auto factList = coursePage->factListView->currentFactList;
+    QTest::mouseClick(((FactList*) factList->layout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(((FactList*) factList->layout->itemAt(1)->widget())->idChildFactMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Check that the fact has moved
+    QVERIFY(factList->layout->count() == 1);
+    QVERIFY(factList->idChildMap.size() == 1);
+    QVERIFY(factList->idChildSectionMap.size() == 1);
+    QVERIFY(factList->idChildFactMap.size() == 0);
+
+    factList = factList->idChildSectionMap.begin()->second;
+    QVERIFY(factList->layout->count() == 3);
+    QVERIFY(factList->idChildMap.size() == 2);
+    QVERIFY(factList->idChildSectionMap.size() == 1);
+    QVERIFY(factList->idChildFactMap.size() == 1);
+
+    // Check that the facts are in the correct order
+    QVERIFY(factList->layout->itemAt(1)->widget() == (QWidget*) factList->idChildSectionMap.begin()->second);
+    QVERIFY(factList->layout->itemAt(2)->widget() == (QWidget*) factList->idChildFactMap.begin()->second);
+}
+
+void SectionTest::test_editSectionOrdering_factOutOfSection()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *level0FactName = "level 0 fact";
+    const char *level0FactType = "Axiom";
+    const char *level1FactName = "level 1 fact";
+    const char *level1FactType = "Axiom";
+    const char *sectionName = "section";
+
+    // Add our course, section and facts
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, level0FactName, level0FactType);
+    TestUtil::addFact(window, sectionName, "Section");
+    TestUtil::addFactToSection(window, level1FactName, level1FactType, sectionName);
+
+    // Move the outer fact inside the section
+    auto factList = coursePage->factListView->currentFactList;
+    QTest::mouseClick(((ExpandingFactWidget*) factList->layout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(factList->idChildSectionMap.begin()->second->idChildFactMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Move the fact back out again
+    QTest::mouseClick(((ExpandingFactWidget*) factList->idChildSectionMap.begin()->second->layout->itemAt(1)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(factList->idChildSectionMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Check that the fact has moved
+    QVERIFY(factList->layout->count() == 2);
+    QVERIFY(factList->idChildMap.size() == 2);
+    QVERIFY(factList->idChildSectionMap.size() == 1);
+    QVERIFY(factList->idChildFactMap.size() == 1);
+
+    factList = factList->idChildSectionMap.begin()->second;
+    QVERIFY(factList->layout->count() == 2);
+    QVERIFY(factList->idChildMap.size() == 1);
+    QVERIFY(factList->idChildSectionMap.size() == 0);
+    QVERIFY(factList->idChildFactMap.size() == 1);
+
+    // Check that the facts are in the correct order
+    factList = coursePage->factListView->currentFactList;
+    QVERIFY(factList->layout->itemAt(0)->widget() == (QWidget*) factList->idChildFactMap.begin()->second);
+    QVERIFY(factList->idChildFactMap.begin()->second->fact.name == level0FactName);
+    QVERIFY(factList->layout->itemAt(1)->widget() == (QWidget*) factList->idChildSectionMap.begin()->second);
+
+    factList = factList->idChildSectionMap.begin()->second;
+    QVERIFY(factList->layout->itemAt(1)->widget() == (QWidget*) factList->idChildFactMap.begin()->second);
+    QVERIFY(factList->idChildFactMap.begin()->second->fact.name == level1FactName);
+}
+
+void SectionTest::test_editSectionOrdering_sectionOutOfSection()
+{
+    CoursePage *coursePage = window->coursePage;
+
+    const char *courseName = "Set Theory";
+    const char *factName = "fact";
+    const char *factType = "Axiom";
+    const char *sectionName0 = "section 0";
+    const char *sectionName1 = "section 1";
+
+    // Add our course, section and facts
+    TestUtil::addCourse(window, courseName);
+    TestUtil::addFact(window, sectionName0, "Section");
+    TestUtil::addFact(window, sectionName1, "Section");
+    TestUtil::addFactToSection(window, factName, factType, sectionName1);
+
+    // Move one section inside the other section
+    auto factList = coursePage->factListView->currentFactList;
+    QTest::mouseClick(((FactList*) factList->layout->itemAt(0)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(((FactList*) factList->layout->itemAt(1)->widget())->idChildFactMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Move the section back out again
+    QTest::mouseClick(((FactList*) factList->idChildSectionMap.begin()->second->layout->itemAt(1)->widget())->moveButton, Qt::LeftButton);
+    QTest::mouseClick(factList->idChildSectionMap.begin()->second->moveAboveButton, Qt::LeftButton);
+
+    // Check that the fact has moved
+    QVERIFY(factList->layout->count() == 2);
+    QVERIFY(factList->idChildMap.size() == 2);
+    QVERIFY(factList->idChildSectionMap.size() == 2);
+    QVERIFY(factList->idChildFactMap.size() == 0);
+
+    factList = (FactList*) factList->layout->itemAt(1)->widget();
+    QVERIFY(factList->layout->count() == 2);
+    QVERIFY(factList->idChildMap.size() == 1);
+    QVERIFY(factList->idChildSectionMap.size() == 0);
+    QVERIFY(factList->idChildFactMap.size() == 1);
+
+    // Check that the facts are in the correct order
+    factList = coursePage->factListView->currentFactList;
+    QVERIFY(((FactList*) factList->layout->itemAt(0)->widget())->sectionNameLabel->text() == sectionName0);
+    QVERIFY(((FactList*) factList->layout->itemAt(1)->widget())->sectionNameLabel->text() == sectionName1);
+
+    factList = (FactList*) factList->layout->itemAt(1)->widget();
+    QVERIFY(factList->layout->itemAt(1)->widget() == (QWidget*) factList->idChildFactMap.begin()->second);
+    QVERIFY(factList->idChildFactMap.begin()->second->fact.name == factName);
+}
+
 void SectionTest::test_deleteSection_empty()
 {
     CoursePage *coursePage = window->coursePage;
--- V3/src/test/section.h
+++ V3/src/test/section.h
@@ -19,6 +19,16 @@ private slots:
 
     void test_editSection();
 
+    void test_editSectionOrdering_moveMode();
+    void test_editSectionOrdering_moveAbove();
+    void test_editSectionOrdering_moveBelow();
+    void test_editSectionOrdering_moveBetween();
+
+    void test_editSectionOrdering_factIntoSection();
+    void test_editSectionOrdering_sectionIntoSection();
+    void test_editSectionOrdering_factOutOfSection();
+    void test_editSectionOrdering_sectionOutOfSection();
+
     void test_deleteSection_empty();
     void test_deleteSection_notEmpty();
 
